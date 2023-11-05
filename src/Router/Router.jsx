f525e9060d8e284ca6630cadd86610093c33d6d2
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-05 06:23:59 +0600

    structure updated

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import AvailableFoods from "../Pages/AvailableFoods";
+import ManageMyFood from "../Pages/ManageMyFood";
+import MyFoodRequest from "../Pages/MyFoodRequest";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,12 +17,23 @@ const router = createBrowserRouter([
       },
       {
         path: "/availablefoods",
+        element: <AvailableFoods />,
       },
       {
         path: "/managemyfoods",
+        element: <ManageMyFood />,
       },
       {
         path: "/myfoodrequest",
+        element: <MyFoodRequest />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
