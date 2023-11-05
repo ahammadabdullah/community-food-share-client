637a819e5ab6ab5e09e84e9cf118aa85bb507238
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-06 00:37:29 +0600

    login register page with authprovider

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,30 +11,43 @@ const NavComp = () => {
           className="mr-3 h-6 sm:h-9"
           alt="community food share logo"
         />
-        {/* <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite React
-        </span> */}
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button>Log in</Button>
+        <Link to={"/login"}>
+          <Button className="hover:!bg-secondary bg-primary ">Log in</Button>
+        </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
         <Link to={"/"}>
-          <Navbar.Link active={location.pathname === "/"}>Home</Navbar.Link>
+          <Navbar.Link
+            className="hover:!text-primary "
+            active={location.pathname === "/"}
+          >
+            Home
+          </Navbar.Link>
         </Link>
         <Link to={"/availablefoods"}>
-          <Navbar.Link active={location.pathname === "/availablefoods"}>
+          <Navbar.Link
+            className="hover:!text-primary "
+            active={location.pathname === "/availablefoods"}
+          >
             Available Foods
           </Navbar.Link>
         </Link>
         <Link to={"/managemyfoods"}>
-          <Navbar.Link active={location.pathname === "/managemyfoods"}>
+          <Navbar.Link
+            className="hover:!text-primary "
+            active={location.pathname === "/managemyfoods"}
+          >
             Manage My Foods
           </Navbar.Link>
         </Link>
         <Link to={"/myfoodrequest"}>
-          <Navbar.Link active={location.pathname === "/myfoodrequest"}>
+          <Navbar.Link
+            className="hover:!text-primary "
+            active={location.pathname === "/myfoodrequest"}
+          >
             My Food Requests
           </Navbar.Link>
         </Link>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./Router/Router";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./Provider/AuthProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
