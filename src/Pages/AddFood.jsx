4a39cb0ee4ba8197bbb461094597c6e7368d838c
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-06 22:18:46 +0600

    fix: add food form

--- src/Pages/AddFood.jsx
+++ src/Pages/AddFood.jsx
@@ -2,13 +2,40 @@ import useAuth from "../Hooks/useAuth";
 
 const AddFood = () => {
   const { user } = useAuth();
+  const handleAddFood = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const foodImage = form.foodImage.value;
+    const foodName = form.foodName.value;
+    const donatorImage = form.donatorImage.value;
+    const donatorName = form.donatorName.value;
+    const foodQuantity = form.foodQuantity.value;
+    const pickupLocation = form.pickupLocation.value;
+    const expiredDateTime = form.expiredDateTime.value;
+    const additionalNotes = form.additionalNotes.value;
+    const donerEmail = form.donerEmail.value;
+    const foodStatus = form.foodStatus.value;
+    const foodData = {
+      foodImage,
+      foodName,
+      donatorImage,
+      donatorName,
+      foodQuantity,
+      pickupLocation,
+      expiredDateTime,
+      additionalNotes,
+      donerEmail,
+      status: foodStatus,
+    };
+    console.log(foodData);
+  };
   return (
     <div className="bg-gray-100 py-10">
       <div className=" w-2/3 mx-auto bg-white rounded-lg px-5">
         <h3 className="text-3xl font-semibold text-primary text-center pt-3">
           Add food
         </h3>
-        <form>
+        <form onSubmit={handleAddFood}>
           <input
             required
             className="w-full bg-gray-100 py-5 pl-5 my-4 rounded-lg text-primary focus:border-primary  focus:outline-none focus:ring-0 "
@@ -45,7 +72,7 @@ const AddFood = () => {
           />
           <input
             className="w-full bg-gray-100 py-5 pl-5 my-4 rounded-lg text-primary focus:border-primary  focus:outline-none focus:ring-0 "
-            type="number"
+            type="text"
             name="pickupLocation"
             placeholder="Pickup Location"
           />
@@ -73,7 +100,7 @@ const AddFood = () => {
             defaultValue={"available"}
             className="w-full bg-gray-100 py-5 pl-5 my-4 rounded-lg text-primary focus:border-primary  focus:outline-none focus:ring-0 "
             type="text"
-            name="status"
+            name="foodStatus"
             placeholder="Status- available / delivered"
           />
           <input
