110980eaa694e52bc7e9e1a39f442a3bd8404ac8
Ahammad Abdulla <ahammadabdullahnew@gmail.com>
2023-11-06 10:19:18 +0600

    featuredfoods complete

--- src/Components/FeaturedFoods/FeaturedFoodCard.jsx
+++ src/Components/FeaturedFoods/FeaturedFoodCard.jsx
@@ -1,7 +1,53 @@
-const FeaturedFoodCard = () => {
+/* eslint-disable react/prop-types */
+const FeaturedFoodCard = ({ food }) => {
+  const {
+    foodImage,
+    foodName,
+    donatorImage,
+    donatorName,
+    foodQuantity,
+    pickupLocation,
+    expiredDateTime,
+    additionalNotes,
+  } = food;
   return (
-    <div className="w-[350px]">
-      <h3></h3>
+    <div className="w-[350px] mx-auto my-3">
+      <div>
+        <img className="w-[350px]" src={foodImage} alt="" />
+      </div>
+      <div>
+        <div className="pt-3 flex justify-between items-center">
+          <img
+            title="Donator Image"
+            className="w-12 rounded-full"
+            src={donatorImage}
+            alt="donator Image"
+          />
+          <h3 title="Donated by" className="font-medium">
+            {donatorName}
+          </h3>
+        </div>
+        <h3 className="text-center text-3xl text-primary font-semibold">
+          {foodName}
+        </h3>
+        <p>
+          Quantity: <span className="font-medium">{foodQuantity}</span>
+        </p>
+        <p>
+          Pickup Location: <span className="font-medium">{pickupLocation}</span>
+        </p>
+        <p>
+          Expiry Date: <span className="font-medium">{expiredDateTime}</span>
+        </p>
+        <p>
+          <span className="font-medium">{additionalNotes}</span>
+        </p>
+        <div className="flex justify-center mt-2">
+          <button className="py-2 px-3 bg-primary hover:bg-secondary text-white">
+            View Details
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/FeaturedFoods/FeaturedFoods.jsx
+++ src/Components/FeaturedFoods/FeaturedFoods.jsx
@@ -1,19 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import FeaturedFoodCard from "./FeaturedFoodCard";
 
 const FeaturedFoods = () => {
   const [featuredFoods, setFeaturedFoods] = useState([]);
+  // const [data, setData] = useState([]);
   useEffect(() => {
-    axios
-      .get("http://localhost:3500/availablefoods")
-      .then((res) => setFeaturedFoods(res.data));
+    axios.get("http://localhost:3500/availablefoods").then((res) => {
+      const data = res.data;
+      const sortedData = data.sort((a, b) => b.foodQuantity - a.foodQuantity);
+
+      return setFeaturedFoods(sortedData);
+    });
   }, []);
   console.log(featuredFoods);
+  // setFeaturedFoods(sortedData);
   return (
     <div className="my-10 max-w-7xl mx-auto">
-      <h3 className="text-center text-5xl font-bold text-primary">
+      <h3 className="pb-5 text-center text-5xl font-bold text-primary">
         Featured Foods
       </h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+        {featuredFoods.slice(0, 6).map((food) => (
+          <FeaturedFoodCard key={food._id} food={food}></FeaturedFoodCard>
+        ))}
+      </div>
     </div>
   );
 };
